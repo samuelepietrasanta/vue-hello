06b364181d215f9121ba49774e80039d69d4b8be
samuelepietrasanta <samuele.pietrasanta@gmail.com>
2021-09-14 14:46:51 +0200

    finished exercise

---
+++ js/myscript.js
@@ -0,0 +1,19 @@
+let titolo = new Vue ({
+
+
+    el : '#titolo',
+
+    data: {
+        message : 'Titolo di prova',
+        classi: 'text-danger text-center',
+    }
+
+})
+
+
+let immagine = new Vue({
+    el : '#immagine-di-prova',
+    data : {
+        immagine: 'img/billie.jpg',
+    },
+})
